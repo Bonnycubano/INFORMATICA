c0ee6ff94a5c1abf109ad221cf5a2e5eba25dd50
Bonnycubano <boniardialessio@gmail.com>
2025-02-11 07:36:55 +0000

    script salutu

--- TPSIT/saluti.sh
+++ TPSIT/saluti.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-
-
-nome=""
-ora=""
-minuti=""
 echo "inserisci il nome"
 read nome
+
 echo "inserisci l'ora"
 read ora
+
 echo "inserisci i minuti"
 read minuti
-if [ ora -gt 12 ];
-then
-    if [ ora -gt 19 ];
+
+if [ $ora -gt 12 ]; then
+    if [ $ora -gt 19 ];
     then
         echo "buonasera $nome! sono le $ora.$minuti."
     else
         echo "buongiorno $nome! sono le $ora.$minuti."
     fi
-elif [ $ora -lt 12 ];
-then   
+elif [ $ora -lt 12 ]; then   
      echo "buongiorno $nome! sono le $ora.$minuti."
 fi
