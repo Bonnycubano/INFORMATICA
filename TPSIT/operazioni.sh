71b0e4d1742187e064440b36e9d93739162a7e02
Bonnycubano <boniardialessio@gmail.com>
2025-03-03 19:12:56 +0000

    controllo

--- TPSIT/operazioni.sh
+++ TPSIT/operazioni.sh
@@ -2,17 +2,14 @@
 
 somma=0
 
-
-if [ "$1" == "add" ]; then
-    i=1
+ i=1
     
     while [ $i -le $# ]; do
         somma=$((somma + ${!i}))  
         i=$((i+1))                   
     done
     
-    echo "Risultato della somma=$somma"
-else
+
     echo "la somma degli argomenti e':  $somma"
-fi
+
 
