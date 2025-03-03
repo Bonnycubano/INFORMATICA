f37659230c4e526f7313e80321d98b4f938455c9
Bonnycubano <boniardialessio@gmail.com>
2025-03-03 18:42:29 +0000

    controllo

---
+++ TPSIT/operazioni.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+somma=0
+
+
+if [ "$1" == "add" ]; then
+    i=2
+    
+    while [ $i -le $# ]; do
+        somma=$((somma + ${!i}))  
+        ((i++))                   
+    done
+    
+    echo "Risultato della somma=$somma"
+else
+    echo "Per favore, usa 'add' come primo argomento."
+fi
+
