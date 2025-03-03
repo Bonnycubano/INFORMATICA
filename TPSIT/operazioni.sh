5e5547906aab68b8cf8bd056d8c91a4b24b82ef9
Bonnycubano <boniardialessio@gmail.com>
2025-03-03 19:23:37 +0000

    nuovo

--- TPSIT/operazioni.sh
+++ TPSIT/operazioni.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-somma=0
+if [ $# -ne 3 ]; then
+    echo "Errore: devi fornire 3 argomenti. Usa il formato:"
+    echo "$0 operazione numero1 numero2"
+    return
+fi
 
- i=1
-    
-    while [ $i -le $# ]; do
-        somma=$((somma + ${!i}))  
-        i=$((i+1))                   
-    done
-    
+operazione=$1
+numero1=$2
+numero2=$3
 
-    echo "la somma degli argomenti e':  $somma"
+
+if [ "$operazione" == "add" ]; then
+    risultato=$((numero1 + numero2))  
+    echo "Risultato della somma: $risultato"
+elif [ "$operazione" == "subtract" ]; then
+    risultato=$((numero1 - numero2))  
+    echo "Risultato della sottrazione: $risultato"
+elif [ "$operazione" == "multiply" ]; then
+    risultato=$((numero1 * numero2))  
+    echo "Risultato della moltiplicazione: $risultato"
+elif [ "$operazione" == "divide" ]; then
+    if [ $numero2 -eq 0 ]; then  
+        echo "Errore: non è possibile dividere per zero!"
+    else
+        risultato=$((numero1 / numero2))  
+        echo "Risultato della divisione: $risultato"
+    fi
+else
+    echo "Operazione non valida. Usa addizzione, sottrazione, moltiplicazione, o divisione."
+fi
+
 
 
